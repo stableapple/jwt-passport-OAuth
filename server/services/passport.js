681728c7267a1a1da8fd96abec5f73c09dbcde09
stableapple <riddhi01.menon@gmail.com>
2021-05-22 22:26:27 +0530

    Added passport

--- server/services/passport.js
+++ server/services/passport.js
@@ -3,3 +3,22 @@ const User = require('../models/user');
 const config = require('../config');
 const JwtStrategy = require('passport-jwt').Strategy
 const ExtractJwt = require('passport-jwt').ExtractJwt
+
+//setup option for jwt strategy
+const jwtOptions = {}
+
+
+//strategy
+const jwtLogin = new JwtStrategy(jwtOptions,function (payload, done){
+    User.findById(payload.subdomains, function(err, user){
+        // error when search failed to occur
+        if(err) { return done(err, false) }
+
+        if(user){
+            done(null, user)
+        }
+        else{
+            done(null, false)
+        }
+    })
+})
